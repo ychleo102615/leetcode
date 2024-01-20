630ba9506e2665d8c32215d9bc2d3f8d10facd7e
ychleo102615 <leo102615@gmail.com>
2024-01-20 17:20:16 +0800

    feat: record, dbc, bx, dbxbc 這種的會失敗

--- 97. Interleaving String/main.ts
+++ 97. Interleaving String/main.ts
@@ -1,7 +1,9 @@
 function isInterleave(s1: string, s2: string, s3: string): boolean {
+    console.log("\n----------- interleave -----------")
     if (s1.length + s2.length != s3.length) {
         return false;
     }
+    console.log("pass count check");
 
     let dstMap = new Map<string, number>();
     let srcMap = new Map<string, number>();
@@ -11,14 +13,12 @@ function isInterleave(s1: string, s2: string, s3: string): boolean {
 
     console.log(dstMap)
     console.log(srcMap)
-    console.log("start", dstMap.get("a"));
     for (let [key, value] of dstMap) {
         if (srcMap.get(key) != value) {
             return false;
-        } else {
-            console.log("check:" + srcMap.get(key))
         }
     }
+    console.log("pass map check");
 
     if (
         (s1[0] != s3[0] && s2[0] != s3[0]) ||
@@ -26,73 +26,100 @@ function isInterleave(s1: string, s2: string, s3: string): boolean {
     ) {
         return false;
     }
+    console.log("pass head tail check")
 
     let i1 = 0;
     let i2 = 0;
     let i3 = 0;
-    let consistentCount = 0;
-    let consParseCount = 0
 
-    let strcmp = () => {
-        let count = 0;
-        while(s1[i1+count] === s2[i2+count] && i1 + count < s1.length) {
-            count++;
+    let strcmp2 = (a: string, b: string) => {
+        let index = 0;
+        while (a[index] == b[index] && index < a.length) {
+            index++;
         }
-        return count
+        return index;
     }
     let recursive = () => {
-        if (i1 == s1.length && i2 == s2.length && i3 == s3.length) {
-            return true;
+        console.log("Into Recursive:");
+        if (i1 == s1.length) {
+            return s2.substring(i2) === s3.substring(i3);
         }
-        if (consistentCount == 0) {
-            consistentCount = strcmp()
+        if (i2 == s2.length) {
+            return s1.substring(i1) === s3.substring(i3);
+        }
+        let ss1 = s1.substring(i1);
+        let ss2 = s2.substring(i2);
+        let ss3 = s3.substring(i3);
+        let consistentCount = strcmp2(ss1, ss2);
+        console.log("substrings:", ss1, ss2, ss3, consistentCount);
+
+        let firstDiff = strcmp2(ss1, ss3);
+        if (firstDiff > 0) {
+            i1 += firstDiff;
+            i3 += firstDiff;
+            if (recursive()) {
+                return true;
+            }
+            i1 -= firstDiff;
+            i3 -= firstDiff;
+            // for (let i = 0; i < firstDiff; i++) {
+            //     i1++;
+            //     i3++;
+            //     if (recursive()) {
+            //         return true;
+            //     }
+            // }
+            // i1 -= firstDiff;
+            // i3 -= firstDiff;
+        }
+
+        if (firstDiff > 0 && consistentCount > 0 && firstDiff < ss2.length) {
+            console.log("Exchange for", firstDiff)
+            i2 += firstDiff;
+            i3 += firstDiff;
+            if (recursive()) {
+                return true;
+            }
+            i2 -= firstDiff;
+            i3 -= firstDiff;
+            // for (let i = 0; i < firstDiff; i++) {
+            //     i2++;
+            //     i3++;
+            //     if (recursive()) {
+            //         return true;
+            //     }
+            // }
+            // i2 -= firstDiff;
+            // i3 -= firstDiff;
         } else {
-            consParseCount++;
-            if (consParseCount > consistentCount) {
-                consistentCount = 0;
-                consParseCount = 0
-                console.log("reset front", i1, i2)
+            let secondDiff = strcmp2(ss2, ss3);
+            if (secondDiff > 0) {
+                // i2 += secondDiff;
+                // i3 += secondDiff;
+                // if (recursive()) {
+                //     return true;
+                // }
+                // i2 -= secondDiff;
+                // i3 -= secondDiff;
+                for (let i = 0; i < secondDiff; i++) {
+                    i2++;
+                    i3++;
+                    if (recursive()) {
+                        return true;
+                    }
+                }
+                i2 -= secondDiff;
+                i3 -= secondDiff;
+
             }
         }
 
-        if (s1[i1] == s3[i3]) {
-            //console.log("enter s1: " + s1[i1]);
-            i1++;
-            i3++;
-            if (recursive()) {
-                return true;
-            }
-            i1--;
-            i3--;
-            //console.log("leave s1: " + s1[i1]);
-        }
-        if (s2[i2] == s3[i3]) {
-            //console.log("enter s2: " + s2[i2]);
-            i2++;
-            i3++;
-            if (recursive()) {
-                return true;
-            }
-            i2--;
-            i3--;
-            //console.log("leave s2: " + s2[i2]);
-        }
-        //console.log(i1, i2, i3);
-
-        if (consistentCount > 0) {
-            i1 -= consistentCount;
-            i2 += consistentCount;
-            //console.log("reset rear", i1, i2, consistentCount)
-            consistentCount = 0;
-            consParseCount = 0;
-            if (recursive()) {
-                return true;
-            }
-        }
         return false;
     }
 
-    return recursive();
+    let result = recursive();
+    console.log("Result:", result);
+    return result;
 };
 
 function addMap(theString: string, theMap: Map<string, number>) {
