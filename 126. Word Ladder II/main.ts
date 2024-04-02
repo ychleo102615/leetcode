a071f3d902ba18783281d66e2850e6805f6148aa
ychleo102615 <leo102615@gmail.com>
2024-04-03 00:16:21 +0800

    feat: pass test

--- 126. Word Ladder II/main.ts
+++ 126. Word Ladder II/main.ts
@@ -2,61 +2,76 @@ function findLadders(beginWord: string, endWord: string, wordList: string[]): st
     if (wordList.includes(beginWord)) {
         wordList.splice(wordList.indexOf(beginWord), 1);
     }
-    wordList.splice(0, 0, beginWord);
-    const paths: string[][] = [];
 
     const endIndex = wordList.indexOf(endWord);
     if (endIndex === -1) {
-        return paths;
+        return [];
     }
 
-    const pathIndexTable: number[][][] = Array.from(Array(wordList.length), () => []);
-
-    const bfsList: number[] = [0]; // index of begin word
+    const bfsList: string[] = [beginWord]; // index of begin word
     let bfsLength = 1;
-    pathIndexTable[0].push([0]);
 
-    const candidates = Array.from(Array(wordList.length).keys());
-    candidates.splice(0, 1);
+    const wordPathMap: Map<string, string[][]> = new Map();
+    wordPathMap.set(beginWord, [[beginWord]]);
+    for (let i = 0; i < wordList.length; i++) {
+        wordPathMap.set(wordList[i], []);
+    }
+    const endWordPath = wordPathMap.get(endWord)!;
 
+    const parentMap: Map<string, string[]> = new Map();
+
+    const leftWordMap: Map<string, number> = new Map();
+    for (let i = 0; i < wordList.length; i++) {
+        leftWordMap.set(wordList[i], i);
+    }
+
+    const ALPHA_COUNT = 26;
+    const LADDER_RANGE = beginWord.length * ALPHA_COUNT;
+    const codeA = ("a").charCodeAt(0);
+
+    // const candidates = Array.from(Array(wordList.length).keys());
+    // candidates.splice(0, 1);
+
+    // let depth = 0;
     while (bfsLength > 0) {
-        // console.log("bfs: ", bfsList.map(x => wordList[x]));
-        // console.log("cand: ", candidates.map(x => wordList[x]));
-        // if (candidates.length === 0) {
-        //     console.log("error")
-        //     break;
-        // }
+        // console.log("depth", depth++)
         for (let i = 0; i < bfsLength; i++) {
-            const cur = bfsList[i];
-
-            for (let j = candidates.length - 1; j >= 0; j--) {
-                const target = candidates[j];
-
-                if (!isReachable(wordList[cur], wordList[target])) {
-                // if (getDiffCount(wordList[cur], wordList[target]) !== 1) {
+            const curWord = bfsList[i];
+            let col: number
+            let searchWord: string
+            for (let j = 0; j < LADDER_RANGE; j++) {
+                col = Math.floor(j / ALPHA_COUNT);
+                searchWord = curWord.slice(0, col) + String.fromCharCode(codeA + j % ALPHA_COUNT) + curWord.slice(col+1);
+                if (!leftWordMap.has(searchWord)) {
                     continue;
                 }
-
-                const pathIndices = pathIndexTable[cur];
-                for (let k = 0; k < pathIndices.length; k++) {
-                    const newPath = pathIndices[k].slice();
-                    newPath.push(target);
-                    pathIndexTable[target].push(newPath);
+                const curPaths = wordPathMap.get(curWord)!;
+                const targetPaths = wordPathMap.get(searchWord)!;
+                // for (let k = 0; k < curPaths.length; k++) {
+                //     const newPath = curPaths[k].slice();
+                //     newPath.push(searchWord);
+                //     targetPaths.push(newPath);
+                // }
+                if (!bfsList.includes(searchWord)) {
+                    bfsList.push(searchWord);
                 }
-
-                if (!bfsList.includes(target)) {
-                    bfsList.push(target);
+                if (!parentMap.has(searchWord)) {
+                    parentMap.set(searchWord, [curWord]);
+                } else {
+                    parentMap.get(searchWord)!.push(curWord);
                 }
             }
         }
         bfsList.splice(0, bfsLength);
         bfsLength = bfsList.length;
-        bfsList.sort();
         for (let i = 0; i < bfsList.length; i++) {
-            candidates.splice(candidates.indexOf(bfsList[i]), 1);
+            leftWordMap.delete(bfsList[i]);
         }
 
-        if (pathIndexTable[endIndex].length > 0 ) {
+        // if (endWordPath.length > 0 ) {
+        //     break;
+        // }
+        if (parentMap.has(endWord)) {
             break;
         }
     }
@@ -64,32 +79,19 @@ function findLadders(beginWord: string, endWord: string, wordList: string[]): st
         * [["aaaaa","aaaaz","aaawz","aavwz","avvwz","vvvwz","vvvww","wvvww","wwvww","wwwww","ywwww","yywww","yyyww","yyyyw","yyyyy","xyyyy","xxyyy","xxxyy","xxxxy","xxxxx","gxxxx","ggxxx","gggxx","ggggx","ggggg"]]
         * */
 
-    for (let i = 0; i < pathIndexTable[endIndex].length; i++) {
-        paths.push(pathIndexTable[endIndex][i].map(x => wordList[x]));
+    // return paths;
+    // return endWordPath;
+    const paths: string[][] = [];
+    const trackParent = function(tracks: string[]) {
+        const parents = parentMap.get(tracks[0]);
+        if (parents === undefined) {
+            paths.push(tracks);
+            return;
+        }
+        for (let i = 0; i < parents.length; i++) {
+            trackParent([parents[i]].concat(tracks))
+        }
     }
-
+    trackParent([endWord]);
     return paths;
 };
-
-function isReachable(a: string, b: string): boolean {
-    let gotOne: boolean | null = null;
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            if (gotOne !== null) {
-                return false;
-            }
-            gotOne = true;
-        }
-    }
-    return gotOne == true;
-}
-
-function getDiffCount(a: string, b: string): number {
-    let count = 0;
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            count++;
-        }
-    }
-    return count;
-}
