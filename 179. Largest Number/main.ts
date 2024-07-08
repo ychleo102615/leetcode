786ae701929930865377c4c5c274dfb34125cdf9
ychleo102615 <leo102615@gmail.com>
2024-07-08 23:11:11 +0800

    feat: opt 179

--- 179. Largest Number/main.ts
+++ 179. Largest Number/main.ts
@@ -1,30 +1,8 @@
 function largestNumber(nums: number[]): string {
     const strnums = nums.map(n => n.toString());
     strnums.sort((a, b) => {
-        let i = 0;
-        while (true) {
-            if (i == a.length && i == b.length) {
-                return 0;
-            } else if (i == a.length) {
-                // 想像這個例子: a = 53, b = 535
-                // 等意於 a = 53, b = 5 ，因為開頭都必為53了
-                b = b.slice(i);
-                i = 0;
-                continue;
-            } else if (i == b.length) {
-                a = a.slice(i);
-                i = 0;
-                continue;
-            }
-            if (a[i] == b[i]) {
-                i++;
-                continue;
-            }
-            return a[i] > b[i] ? -1 : 1;
-        }
+        return a + b > b + a ? -1 : 1;
     })
-    if (parseInt(strnums.join("")) == 0) {
-        return "0"
-    }
+    if (strnums[0] == "0") return "0";
     return strnums.join("");
 };
