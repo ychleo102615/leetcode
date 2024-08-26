75c1ea56a2197c83b59d5514e21ee06e02e2bcfc
ychleo102615 <leo102615@gmail.com>
2024-08-26 22:27:24 +0800

    feat: opt 424

--- 424. Longest Repeating Character Replacement/main.ts
+++ 424. Longest Repeating Character Replacement/main.ts
@@ -1,79 +1,25 @@
 function characterReplacement(s: string, k: number): number {
-    const alphabetTrack = new Map<string, number[]>();
-
-    let cur = "";
-    for (let i = 0; i < s.length; i++) {
-        if (cur != s[i]) {
-            if (alphabetTrack.has(cur)) {
-                alphabetTrack.get(cur)!.push(0);
-            }
-            cur = s[i];
-            if (alphabetTrack.has(cur)) {
-                alphabetTrack.get(cur)!.push(0);
-            } else {
-                alphabetTrack.set(cur, [i, 0]);
-            }
-        }
-        alphabetTrack.forEach((track) => {
-            track[track.length-1]++;
-        });
-    }
-    alphabetTrack.get(cur)!.push(0);
-    // console.log(alphabetTrack);
-
     let maxLen = 0;
+    const curCharCount = new Array(26).fill(0); // 目前window內的字母數量
+    const A = 'A'.charCodeAt(0);
 
-    alphabetTrack.forEach((track, alphabet) => {
-        // console.log("alphabet", alphabet)
+    for (let end = 0, start = 0, maxCount = 0; end < s.length; end++) {
+        let char = s.charCodeAt(end) - A;
 
-        let start = 1;
-        let end = 1;
-        let coda = k;
-        let len = track[start];
+        // 同一個window內，最多的字母數量
+        maxCount = Math.max(maxCount, ++curCharCount[char]);
 
-        function checkLen() {
-            const possibleAddOn = Math.min(track[start-1], coda);
-            if (len + possibleAddOn > maxLen) {
-                maxLen = len + possibleAddOn;
-            }
+        // 如果window內的字母數量 + k < window長度，代表無法全部替換
+        /*
+            * 這個瞬間，所能獲得的最大長度為 maxCount + k
+            */
+        if (end - start + 1 - maxCount > k) {
+            curCharCount[char]--;
+            start++;
         }
 
-        while (end < track.length) {
-            checkLen();
-
-            if (coda >= track[end+1]) {
-                coda -= track[end+1];
-                len += track[end+1] + (track[end+2] | 0);
-                end += 2;
-            } else if (start + 2 > end) {
-                start += 2;
-                end += 2;
-                if (end >= track.length) {
-                    break;
-                }
-                len = track[start];
-            } else {
-
-                // if (coda > 0) {
-                //     len += Math.min(track[start-1], coda);
-                // }
-                // if (len > maxLen) {
-                //     maxLen = len;
-                // }
-                // if (coda > 0) {
-                //     len -= Math.min(track[start-1], coda);
-                // }
-
-                coda += track[start+1];
-                len -= track[start] + track[start+1];
-                start += 2;
-            }
-        }
-
-        checkLen();
-
-
-    });
+        maxLen = Math.max(maxLen, end - start + 1);
+    }
 
     return maxLen;
 };
